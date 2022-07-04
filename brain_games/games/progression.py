edd87af783e849118da7888926b4346a51954394
NikolyKa <karabakin.kolia@mail.ru>
2022-07-04 16:01:37 +0200

    add last changes

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,14 +5,15 @@ START_FIRST_NUM = 1
 END_FIRST_NUM = 20
 START_STEP_NUM = 2
 END_STEP_NUM = 10
-
-
+FIRST_INDEX = 0
+LAST_INDEX = 9
+NUMBER_OF_VARIABLES = 10
 def start():
     first_num = randint(START_FIRST_NUM, END_FIRST_NUM)
     step_num = randint(START_STEP_NUM, END_STEP_NUM)
     line = ''
-    index = randint(0, 9)
-    for i in range(10):
+    index = randint(FIRST_INDEX, LAST_INDEX)
+    for i in range(NUMBER_OF_VARIABLES):
         first_num += step_num
         line += str(first_num) + ' '
     new = line.split(" ")
